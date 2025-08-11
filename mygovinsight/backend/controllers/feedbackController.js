5773b67fc84c215447eb6bf0ac38b4d31d964447
robel <robelwondwosen247@gmail.com>
2025-08-11 16:38:52 +0300

    feedbackcontroller added

---
+++ mygovinsight/backend/controllers/feedbackController.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router()
+
+const Feedback = require('../models/Feedback');
+
+exports.createFeedback = async(req, res) => {
+    try{
+        const feedback = new Feedback ({
+            user: req.user.id,
+            sector: req.body.sector,
+            institution: req.body.institution,
+            description: req.body.description,
+            attach: req.body.attach,
+            rating:req.body.rating
+        });
+        const savedfeedback = await feedback.save();
+        res.status(201).json({
+            success: true,
+            data: savedfeedback
+        });   
+    }   catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+exports.getAllFeedback = async(req, res) => {
+    try{
+        const feedbacks = await Feedback.find().populate('user', 'name email');
+        res.status(200).json({
+            success: true,
+            count: feedbacks.length,
+            data: feedbacks
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+exports.getFeedbackById = async(req, res) => {
+    try{
+        const feedback = await Feedback.findById(req.params.id).populate('user', 'name email');
+        if(!feedback){
+            return res.status(404).json({
+             success: false,
+             message: 'feedback not found'
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: feedback
+        });
+    }   catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+exports.deleteFeedbackById = async (req, res) => {
+    try{
+        const feedback = await Feedback.findByIdAndDelete(req.params.id);
+        if (!feedback){
+            return res.status(404).json({
+                success: false,
+                message: 'feedback not found'
+            });
+        } 
+        res.status(200).json({
+            success: true,
+            message: 'feedback deleted successfully'
+        });
+    }   catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
